5de24249ba027e7f8afa77072f6c5f4effe4526e
dave vebber <davevebber@gmail.com>
2021-05-26 13:59:21 -0700

    you can add notes and delete notes and wrtie notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,58 @@
-import './App.css';
+import { useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+
+    const [notes, setNotes] = useState([]);
+
+    const [activeNote, setActiveNote] = useState(false);
+
+    // click button to add note
+    const onAddNote = () => {
+
+        let newNote = {
+            id: uuid(),
+            title: "untitled note",
+            body: "",
+            lastModified: Date.now(),
+        };
+
+        setNotes([newNote, ...notes]);
+    };
+
+    // click button to delete note
+    const onDeleteNote = (idToDelete) => {
+        setNotes(notes.filter((note) => note.id !== idToDelete))
+    }
+
+    // shows the current note on the preview page
+    const getActiveNote = () => {
+        return notes.find((note) => note.id === activeNote);
+    }
+
+    return (
+
+        <div className="App">
+
+            <Sidebar
+            notes={notes}
+            
+            onAddNote={onAddNote}
+
+            onDeleteNote={onDeleteNote}
+
+            activeNote={activeNote}
+
+            setActiveNote={setActiveNote}
+             />
+
+            <Main activeNote={getActiveNote()} />
+
+        </div>
+    )
 }
 
 export default App;
